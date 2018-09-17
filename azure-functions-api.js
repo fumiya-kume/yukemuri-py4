187584831e7d19c19078c60f5df92cd96fac55d9
Fumiya Kume <fumiya.kume@hotmail.com>
2018-09-17 13:15:06 +0900

    Update azure-functions-api.js

--- azure-functions-api.js
+++ azure-functions-api.js
@@ -1,7 +1,8 @@
 const fetch = require('node-fetch');
 
 module.exports = function (context, req) {
-    fetch('https://teratail.com/api/v1/tags/ruby/questions')
+    const tagName = req.query.tagName;
+    fetch('https://teratail.com/api/v1/tags/" + tagName + "/questions')
         .then(response => response.json())
         .then(json => {
             context.res = { body: json };
