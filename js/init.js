cef66714be72baf5c4c8e3e8879c252116877854
vivid344 <vivid344@icloud.com>
2018-09-17 12:18:50 +0900

    [add]検索候補の追加

--- js/init.js
+++ js/init.js
@@ -1,12 +1,12 @@
 function init(){
-    url = 'https://teratail-loader.azurewebsites.net/api/HttpTriggerJS1?code=qd9LIjalN5rdlsXxvpRvsmDrbHl8BMCMAv3wr1SMlrCQhagxmuIL7g==&tagId=' + 5;
+    url = 'https://teratail-loader.azurewebsites.net/api/HttpTriggerJS1?code=qd9LIjalN5rdlsXxvpRvsmDrbHl8BMCMAv3wr1SMlrCQhagxmuIL7g==&tagId=' + 3;
     $.ajax({
         url: url,
         type: "GET",
         async: true
     }).done(function (data, textStatus, jqXHR) {
         for (x = 0; x < data.length; x++){
-            $('#' + 5).append('<div class="column-content" data-url="' + data[x].link + '">'+ data[x].title +'</div>');
+            $('#' + 3).append('<div class="column-content" data-url="' + data[x].link + '">'+ data[x].title +'</div>');
         }
     }).fail(function (jqXHR, textStatus, errorThrown) {
         alert("通信に失敗しました。再読込して下さい。");
