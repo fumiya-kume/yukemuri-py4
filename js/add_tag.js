f7fdec2cbb0232fd67c89efb774eea0444e6ddd1
vivid344 <vivid344@icloud.com>
2018-09-17 11:10:48 +0900

    [fix]cssのバグ，削除ボタンのバグを実装

--- js/add_tag.js
+++ js/add_tag.js
@@ -1,19 +1,27 @@
 function add_tag() {
-    $(".tag").toggle();
+    $(".tag-holder").toggle();
 }
 
-function tag(i,name){
-    $(".tag").toggle();
+function tag(i, name) {
+    $(".tag-holder").toggle();
     url = 'https://teratail-loader.azurewebsites.net/api/HttpTriggerJS1?code=qd9LIjalN5rdlsXxvpRvsmDrbHl8BMCMAv3wr1SMlrCQhagxmuIL7g==&tagId=' + i;
     $.ajax({
         url: url,
         type: "GET",
         async: true
     }).done(function (data, textStatus, jqXHR) {
-        $('.column-holder').append('<div class="column" id= '+ i + '><div class="column-header"><div class="title"> '+ name + '</div></div></div>');
-        for (x = 0; x < data.length; x++){
-            $('#' + i).append('<div class="column-content" data-url="' + data[x].link + '">' + data[x].title +'</div>');
+        var list_holder_width = $('.list-holder').width();
+        $('.list-holder').css({width: list_holder_width + 300});
+        $('.tag[data-id=' + i + ']').addClass('tag-hidden');
+        $('.column-holder').append('<div class="column" data-columnid="'+ i +'"><div class="column-header"><div class="title"> ' + name + '</div><i class="fas fa-times cross" onclick="delete_tag('+ i +')"></i></div><ul class="column-contents" id="' + i + '"> </ul></div>');
+        for (x = 0; x < data.length; x++) {
+            $('#' + i).append('<div class="column-content" data-url="' + data[x].link + '">' + data[x].title + '</div>');
         }
     }).fail(function (jqXHR, textStatus, errorThrown) {
     });
+}
+
+function delete_tag(i){
+    $('.column[data-columnid='+ i +']').remove();
+    $('.tag[data-id=' + i + ']').removeClass('tag-hidden');
 }
