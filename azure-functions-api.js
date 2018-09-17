50accb8f697cd6bd5aa2780de1c0e55a41b132ce
Fumiya Kume <fumiya.kume@hotmail.com>
2018-09-17 11:57:17 +0900

    Update azure-functions-api.js

--- azure-functions-api.js
+++ azure-functions-api.js
@@ -1,22 +1,10 @@
-const FeedParser = require('feedparser');
-const Request = require('request');
 const fetch = require('node-fetch');
 
 module.exports = function (context, req) {
-
-    // const hoge = async () => {
-
-    // }
-
-    function SearchTeratail() {
-        fetch("https://teratail.com/api/v1/tags/ruby/questions")
-            .then(res => {
-                context.res = { body: res.json() };
-                context.done();
-            })
-    }
-
-    SearchTeratail()
-    // context.res = { body: "response" };
-    // context.done();
+    fetch('https://teratail.com/api/v1/tags/ruby/questions')
+        .then(response => response.json())
+        .then(json => {
+            context.res = { body: json };
+            context.done();
+        })
 };
